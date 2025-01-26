6e911d7bc3b73023bc165a4c8f528a39c763116d
Aleksandar <achernozemski@dxasoft.com>
2025-01-26 02:08:29 +0200

    Reset level #28

--- Assets/Scripts/GlobalBehaviour.cs
+++ Assets/Scripts/GlobalBehaviour.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GlobalBehaviour : MonoBehaviour
 {
     private static GlobalBehaviour instance;
     public AudioSource audioSource, audioSourceBubble;
     public Image brightness;
+
+    public GameObject resetShow;
+    private float resetTime;
 
     public static GlobalBehaviour Instance => instance;
 
@@ -21,5 +26,21 @@ public class GlobalBehaviour : MonoBehaviour
 
         instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    public void Update()
+    {
+      if (Input.GetKey(KeyCode.X))
+      {
+        resetShow.SetActive(true);
+        resetTime += Time.deltaTime;
+        if (resetTime >= 5)
+          SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+      }
+      else
+      {
+        resetShow.SetActive(false);
+        resetTime = 0;
+      }
     }
 }
