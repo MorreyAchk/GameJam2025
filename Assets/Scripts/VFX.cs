7917084d9d5198d25eb10c89e17b80662a3fcb71
Yasen <yasenyankov21@gmail.com>
2025-03-18 20:29:10 +0200

    Small audio adjustments

--- Assets/Scripts/VFX.cs
+++ Assets/Scripts/VFX.cs
@@ -11,6 +11,21 @@ public class VFX : MonoBehaviour
     {
         audioSource = GetComponent<AudioSource>();
         audioSource.volume = PlayerPrefs.GetFloat("vfx");
+        AudioReverbFilter audioReverb = gameObject.AddComponent<AudioReverbFilter>();
+        audioReverb.dryLevel = 0;
+        audioReverb.room = -1000;
+        audioReverb.roomHF = -100;
+        audioReverb.roomLF = 0;
+        audioReverb.decayTime = 1.49f;
+        audioReverb.decayHFRatio = 0.83f;
+        audioReverb.reflectionsLevel = -2602;
+        audioReverb.reflectionsDelay = 0;
+        audioReverb.reverbLevel = 25;
+        audioReverb.reverbDelay = 0.011f;
+        audioReverb.hfReference = 5000;
+        audioReverb.lfReference = 250;
+        audioReverb.diffusion = 100;
+        audioReverb.density = 50;
     }
 
     internal void UpdateVolume(float value)
